6606c2fea7bcb5d5a95dc8fd9b9a4bf0750b7956
Jon Cram <jon@webignition.net>
2012-08-15 20:44:56 +0100

    Updated to external string parser package

--- src/webignition/QuotedString/Parser.php
+++ src/webignition/QuotedString/Parser.php
@@ -2,111 +2,65 @@
 
 namespace webignition\QuotedString;
 
+use webignition\StringParser\StringParser;
+
 /**
  * Parse a given input string into a QuotedString
  * 
  */
-class Parser {
+class Parser extends StringParser {
     
     const QUOTE_DELIMITER = '"';
     const ESCAPE_CHARACTER = '\\';
+    
     const STATE_IN_QUOTED_STRING = 1;
     const STATE_LEFT_QUOTED_STRING = 2;
-    const STATE_UNKNOWN = 3;
-    const STATE_INVALID_LEADING_CHARACTERS = 4;
-    const STATE_INVALID_TRAILING_CHARACTERS = 5;      
-    const STATE_ENTERING_QUOTED_STRING = 6;
-    const STATE_INVALID_ESCAPE_CHARACTER = 7;
+    const STATE_INVALID_LEADING_CHARACTERS = 3;
+    const STATE_INVALID_TRAILING_CHARACTERS = 4;      
+    const STATE_ENTERING_QUOTED_STRING = 5;
+    const STATE_INVALID_ESCAPE_CHARACTER = 6;
     
-    /**
-     *
-     * @var int
-     */
-    private $currentState = self::STATE_UNKNOWN;
-    
-    
-    /**
-     *
-     * @var string
-     */
-    private $inputString;
-    
-    
-    /**
-     *
-     * @var string
-     */
-    private $outputString;
-    
-    
-    /**
-     * Pointer to position of current character
-     * 
-     * @var int
-     */
-    private $currentCharacterIndex = 0;
-    
-    
-    /**
-     *
-     * @var int
-     */
-    private $inputStringLength = 0;
-    
-    
-    /**
-     *
-     * @param type $inputString 
-     */
     public function parse($inputString) {
-        $this->inputString = trim($inputString);
-        $this->inputStringLength = strlen($inputString);
-        
-        while ($this->currentCharacterIndex < $this->inputStringLength) {            
-            $this->parseCurrentCharacter();
-        }
-
-        return new QuotedString($this->outputString);
+        return new QuotedString(parent::parse($inputString));        
     }
     
-    
-    private function parseCurrentCharacter() {       
-        switch ($this->currentState) {            
+    protected function parseCurrentCharacter() {       
+        switch ($this->getCurrentState()) {            
             case self::STATE_ENTERING_QUOTED_STRING:
-                $this->currentCharacterIndex++;
-                $this->currentState = self::STATE_IN_QUOTED_STRING;
+                $this->incrementCurrentCharacterPointer();
+                $this->setCurrentState(self::STATE_IN_QUOTED_STRING);
                 break;             
             
             case self::STATE_IN_QUOTED_STRING:
                 if ($this->isCurrentCharacterQuoteDelimiter()) {                    
                     if ($this->isPreviousCharacterEscapeCharacter()) {
-                        $this->outputString .= $this->getCurrentCharacter();
-                        $this->currentCharacterIndex++;
+                        $this->appendOutputString();
+                        $this->incrementCurrentCharacterPointer();
                     } else {
-                        $this->currentState = self::STATE_LEFT_QUOTED_STRING;
-                        $this->currentCharacterIndex++;
+                        $this->setCurrentState(self::STATE_LEFT_QUOTED_STRING);     
+                        $this->incrementCurrentCharacterPointer();
                     }
                 }
                 
                 if ($this->isCurrentCharacterEscapeCharacter()) {
                     if ($this->isNextCharacterQuoteCharacter()) {
-                        $this->currentCharacterIndex++;                        
+                        $this->incrementCurrentCharacterPointer();                       
                     } else {
-                        $this->currentState = self::STATE_INVALID_ESCAPE_CHARACTER;
+                        $this->setCurrentState(self::STATE_INVALID_ESCAPE_CHARACTER);
                     }
                 }
                
                 if (!$this->isCurrentCharacterQuoteDelimiter() && !$this->isCurrentCharacterEscapeCharacter()) {
-                    $this->outputString .= $this->getCurrentCharacter();
-                    $this->currentCharacterIndex++;
+                    $this->appendOutputString();
+                    $this->incrementCurrentCharacterPointer();
                 }
                 
                 break;
             
             case self::STATE_LEFT_QUOTED_STRING:
-                if ($this->currentCharacterIndex < $this->inputStringLength - 1) {
-                    $this->currentState = self::STATE_INVALID_TRAILING_CHARACTERS;
-                    $this->currentCharacterIndex++;
+                if (!$this->isCurrentCharacterLastCharacter()) {
+                    $this->setCurrentState(self::STATE_INVALID_TRAILING_CHARACTERS);
+                    $this->incrementCurrentCharacterPointer();
                 }
                 
                 break;
@@ -120,23 +74,23 @@ class Parser {
                 break;
             
             case self::STATE_INVALID_ESCAPE_CHARACTER:
-                throw new Exception('Invalid escape character at position '.$this->currentCharacterIndex, 3);
+                throw new Exception('Invalid escape character at position '.$this->getCurrentCharacterPointer(), 3);
                 break;
             
             case self::STATE_INVALID_TRAILING_CHARACTERS:
-                throw new Exception('Invalid trailing characters after last quote character at position '.$this->currentCharacterIndex.' - did you forget to escape an internal quote?', 2);
+                throw new Exception('Invalid trailing characters after last quote character at position '.$this->getCurrentCharacterPointer().' - did you forget to escape an internal quote?', 2);
                 break;                
         }
     }
-    
+
     
     private function deriveCurrentState() {
         if ($this->isCurrentCharacterFirstCharacter()) {
             if ($this->isCurrentCharacterQuoteDelimiter()) {
-                return $this->currentState = self::STATE_ENTERING_QUOTED_STRING;
+                return $this->setCurrentState(self::STATE_ENTERING_QUOTED_STRING);
             }
             
-            return $this->currentState = self::STATE_INVALID_LEADING_CHARACTERS;
+            return $this->setCurrentState(self::STATE_INVALID_LEADING_CHARACTERS);
         }
     }
     
@@ -173,51 +127,8 @@ class Parser {
      */
     private function isNextCharacterQuoteCharacter() {
         return $this->getNextCharacter() == self::QUOTE_DELIMITER;
-    }
+    }  
     
-    
-    /**
-     *
-     * @return boolean 
-     */
-    private function isCurrentCharacterFirstCharacter() {
-        if ($this->currentCharacterIndex != 0) {
-            return false;
-        }
-        
-        return !is_null($this->getCurrentCharacter());
-    }
-    
-    
-    /**
-     *
-     * @return string
-     */
-    private function getCurrentCharacter() {
-        return ($this->currentCharacterIndex < $this->inputStringLength) ? $this->inputString[$this->currentCharacterIndex] : null;
-    }   
-    
-    
-    /**
-     *
-     * @return string
-     */
-    private function getPreviousCharacter() {
-        if ($this->currentCharacterIndex == 0) {
-            return null;
-        }
-        
-        $previousCharacterIndex = $this->currentCharacterIndex - 1;
-        return ($previousCharacterIndex > $this->inputStringLength) ? null : $this->inputString[$previousCharacterIndex];
-    }
-    
-    
-    /**
-     *
-     * @return string
-     */
-    private function getNextCharacter() {        
-        return ($this->currentCharacterIndex == $this->inputStringLength - 1) ? null : $this->inputString[$this->currentCharacterIndex + 1];
-    }
+
 }
  
