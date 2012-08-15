7baeea8089fa3157f3041cb7d7f3e36e09455497
Jon Cram <jon@webignition.net>
2012-08-15 20:53:47 +0100

    Be more explicit in tests regarding expected exceptions

--- tests/QuotedStringParserTest.php
+++ tests/QuotedStringParserTest.php
@@ -12,20 +12,26 @@ class QuotedStringParserTest extends PHPUnit_Framework_TestCase {
         $parser = new \webignition\QuotedString\Parser();
         
         try {
-           $parser->parse('one two three');
-           $this->fail('Invalid leading character exception not thrown');           
+           $parser->parse('one two three');                     
         } catch (\webignition\QuotedString\Exception $exception) {
-        }                 
+            $this->assertEquals(1, $exception->getCode());
+            return;
+        }          
+        
+        $this->fail('Invalid leading character exception not thrown'); 
     }    
     
     public function testParseWithInvalidTrailingCharacters() {
         $parser = new \webignition\QuotedString\Parser();
         
         try {
-           $parser->parse('"one two three" invalid trailing characters');
-           $this->fail('Invalid trailing character exception not thrown');           
+           $parser->parse('"one two three" invalid trailing characters');                     
         } catch (\webignition\QuotedString\Exception $exception) {
+            $this->assertEquals(2, $exception->getCode());
+            return;
         }         
+        
+        $this->fail('Invalid trailing character exception not thrown'); 
     }
     
     public function testParseWithEscapedInternalQuote() {
@@ -38,10 +44,13 @@ class QuotedStringParserTest extends PHPUnit_Framework_TestCase {
         $parser = new \webignition\QuotedString\Parser();
         
         try {
-           $parser->parse('"one \two three"');
-           $this->fail('Invalid escape character exception not thrown');           
+           $parser->parse('"one \two three"');                     
         } catch (\webignition\QuotedString\Exception $exception) {
+            $this->assertEquals(3, $exception->getCode());
+            return;
         }
+        
+        $this->fail('Invalid escape character exception not thrown'); 
     }
     
     
