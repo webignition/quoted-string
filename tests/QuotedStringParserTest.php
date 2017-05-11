63f4d0312f640247c684c595070da09e4e57e260
Jon Cram <jon@webignition.net>
2017-05-11 12:30:39 +0100

    Add PSR2 CS and PSR4 autoloading

--- tests/QuotedStringParserTest.php
+++ tests/QuotedStringParserTest.php
@@ -1,58 +1,92 @@
 <?php
 
-class QuotedStringParserTest extends PHPUnit_Framework_TestCase {
+namespace webignition\Tests\QuotedString;
 
-    public function testParseSimpleQuotedString() {
-        $parser = new \webignition\QuotedString\Parser();
-        $quotedString = $parser->parse('"one two three"');
-        $this->assertEquals('one two three', $quotedString->getValue());
+use PHPUnit_Framework_TestCase;
+use webignition\QuotedString\Exception;
+use webignition\QuotedString\Parser;
+use webignition\QuotedString\QuotedString;
+
+class QuotedStringParserTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Parser
+     */
+    private $parser;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->parser = new Parser();
     }
-    
-    public function testParseWithInvalidLeadingCharacters() {
-        $parser = new \webignition\QuotedString\Parser();
-        
-        try {
-           $parser->parse('one two three');                     
-        } catch (\webignition\QuotedString\Exception $exception) {
-            $this->assertEquals(1, $exception->getCode());
-            return;
-        }          
-        
-        $this->fail('Invalid leading character exception not thrown'); 
-    }    
-    
-    public function testParseWithInvalidTrailingCharacters() {
-        $parser = new \webignition\QuotedString\Parser();
-        
-        try {
-           $parser->parse('"one two three" invalid trailing characters');                     
-        } catch (\webignition\QuotedString\Exception $exception) {
-            $this->assertEquals(2, $exception->getCode());
-            return;
-        }         
-        
-        $this->fail('Invalid trailing character exception not thrown'); 
+
+    /**
+     * @dataProvider parseInvalidInputDataProvider
+     *
+     * @param string $input
+     * @param string $expectedExceptionMessage
+     * @param int $expectedExceptionCode
+     */
+    public function testParseInvalidInput($input, $expectedExceptionMessage, $expectedExceptionCode)
+    {
+        $this->setExpectedException(
+            Exception::class,
+            $expectedExceptionMessage,
+            $expectedExceptionCode
+        );
+
+        $this->parser->parse($input);
     }
-    
-    public function testParseWithEscapedInternalQuote() {
-        $parser = new \webignition\QuotedString\Parser();
-        $quotedString = $parser->parse('"one \"two\" three"');
-        $this->assertEquals('one "two" three', $quotedString->getValue());
+
+    public function parseInvalidInputDataProvider()
+    {
+        return [
+            'invalid leading characters' => [
+                'input' => 'foo',
+                'expectedExceptionMessage' => 'Invalid leading characters before first quote character',
+                'expectedExceptionCode' => 1,
+            ],
+            'invalid trailing characters' => [
+                'input' => '"foo" bar',
+                'expectedExceptionMessage' => 'Invalid trailing characters after last quote character at position 7',
+                'expectedExceptionCode' => 2,
+            ],
+            'invalid escape characters' => [
+                'input' => '"foo \bar"',
+                'expectedExceptionMessage' => 'Invalid escape character at position 5',
+                'expectedExceptionCode' => 3,
+            ],
+        ];
     }
-    
-    public function testParseWithInvalidEscapeCharacter() {
-        $parser = new \webignition\QuotedString\Parser();
-        
-        try {
-           $parser->parse('"one \two three"');                     
-        } catch (\webignition\QuotedString\Exception $exception) {
-            $this->assertEquals(3, $exception->getCode());
-            return;
-        }
-        
-        $this->fail('Invalid escape character exception not thrown'); 
+
+    /**
+     * @dataProvider parseValidInputDataProvider
+     *
+     * @param string $input
+     * @param string $expectedValue
+     */
+    public function testParseValidInput($input, $expectedValue)
+    {
+        $quotedString = $this->parser->parse($input);
+
+        $this->assertInstanceOf(QuotedString::class, $quotedString);
+        $this->assertEquals($expectedValue, (string)$quotedString->getValue());
     }
-    
-    
-    
+
+    /**
+     * @return array
+     */
+    public function parseValidInputDataProvider()
+    {
+        return [
+            'without inner quotes' => [
+                'input' => '"foo"',
+                'expectedValue' => 'foo',
+            ],
+            'with inner quotes' => [
+                'input' => '"foo \"bar\" foobar"',
+                'expectedValue' => 'foo "bar" foobar',
+            ],
+        ];
+    }
 }
--- tests/QuotedStringTest.php
+++ tests/QuotedStringTest.php
@@ -1,19 +1,39 @@
 <?php
 
-class QuotedStringTest extends PHPUnit_Framework_TestCase {
+namespace webignition\Tests\QuotedString;
 
-    public function testWithoutEnclosedQuotes() {
-        $quotedString = new \webignition\QuotedString\QuotedString();
-        $quotedString->setValue('value');
-        
-        $this->assertEquals('"value"', (string)$quotedString);
+use PHPUnit_Framework_TestCase;
+use webignition\QuotedString\QuotedString;
+
+class QuotedStringTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider castToStringDataProvider
+     *
+     * @param $input
+     * @param $expectedQuotedString
+     */
+    public function testCastToString($input, $expectedQuotedString)
+    {
+        $quotedString = new QuotedString($input);
+
+        $this->assertEquals($expectedQuotedString, (string)$quotedString);
     }
-    
-    
-    public function testWithEnclosedQuotes() {
-        $quotedString = new \webignition\QuotedString\QuotedString();
-        $quotedString->setValue('v"al"ue');
-        
-        $this->assertEquals('"v\"al\"ue"', (string)$quotedString);        
+
+    /**
+     * @return array
+     */
+    public function castToStringDataProvider()
+    {
+        return [
+            'without inner quotes' => [
+                'value' => 'foo',
+                'expectedQuotedString' => '"foo"',
+            ],
+            'with inner quotes' => [
+                'value' => 'f"o"o',
+                'expectedQuotedString' => '"f\"o\"o"',
+            ],
+        ];
     }
 }
